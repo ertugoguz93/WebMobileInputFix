1a25395d76485ba38c69d2646a3eb34c24a3efd8
ertugoguz93 <ertug@hazmob.net>
2025-03-27 15:01:22 +0300

    Update FixIndexFile.cs

--- Assets/KeyboardWebGL/Editor/FixIndexFile.cs
+++ Assets/KeyboardWebGL/Editor/FixIndexFile.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 using UnityEditor.Callbacks;
 using System.IO;
 
@@ -10,19 +12,31 @@ namespace WebGLKeyboard
     /// <summary>
     /// Edits the index.html file (the Unity default one) after the build to add the necessary elements to the fix work
     /// </summary>
-    public class FixIndexFile : MonoBehaviour
+    public class FixIndexFile : IPostprocessBuildWithReport
     {
-        [PostProcessBuild(1)]
-        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        public int callbackOrder { get { return 0; } }
+
+        public void OnPostprocessBuild(BuildReport report)
         {
-            if (target == BuildTarget.WebGL)
+            if (report.summary.platform == BuildTarget.WebGL)
             {
-                string text = File.ReadAllText(Path.Combine(pathToBuiltProject, "index.html"));
-                string original = "<div class=\"webgl-content\">";
-                string replace = "<script>\r\n\t\tfunction FixInputOnSubmit() {\r\n\t\t\tdocument.getElementById(\"fixInput\").blur();\r\n\t\t\tevent.preventDefault();\r\n\t\t}\r\n\t</script>\r\n    <div>\r\n\t\t<form onsubmit=\"FixInputOnSubmit()\" autocomplete=\"off\" style=\"width: 0px; height: 0px; position: absolute; top: -9999px;\">\r\n\t\t\t<input type=\"text\" id=\"fixInput\" oninput=\"gameInstance.Module.ccall('FixInputUpdate', null)\" onblur=\"gameInstance.Module.ccall('FixInputOnBlur', null)\" style=\"font-size: 42px;\">\r\n\t\t</form>\r\n\t</div>\r\n\t<div class=\"webgl-content\">";
-                
-                text = text.Replace(original, replace);
-                File.WriteAllText(Path.Combine(pathToBuiltProject, "index.html"), text);
+                string text = File.ReadAllText(Path.Combine(report.summary.outputPath, "index.html"));
+
+                string[] injection = {
+                    "\t\t<script>\n\t\t\tvar gameInstance = undefined;\n\n\t\t\tfunction FixInputOnSubmit() {\n\t\t\t\tdocument.getElementById(\"fixInput\").blur();\n\t\t\t\tevent.preventDefault();\n\t\t\t}\n\t\t</script>\n\t\t<div>\n\t\t\t<form onsubmit=\"FixInputOnSubmit()\" autocomplete=\"off\" style=\"width: 0px; height: 0px; position: absolute; top: -9999px;\">\n\t\t\t\t<input type=\"text\" id=\"fixInput\" oninput=\"gameInstance.Module.asmLibraryArg._FixInputUpdate()\" onblur=\"gameInstance.Module.asmLibraryArg._FixInputOnBlur()\" style=\"font-size: 42px;\">\n\t\t\t</form>\n\t\t</div>\n",
+                    "\t\t\t\t\tgameInstance = unityInstance;\n"
+                };
+
+                text = text.Replace(
+                    "<body>\n",
+                    "<body>\n" + injection[0]
+                );
+                text = text.Replace(
+                    "}).then((unityInstance) => {\n",
+                    "}).then((unityInstance) => {\n" + injection[1]
+                );
+
+                File.WriteAllText(Path.Combine(report.summary.outputPath, "index.html"), text);
             }
         }
     }
